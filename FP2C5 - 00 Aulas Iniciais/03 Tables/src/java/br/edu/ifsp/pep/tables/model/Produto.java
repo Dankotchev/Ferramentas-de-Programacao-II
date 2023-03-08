829aa301b13bb4e0da43fa8627a07e360096c2ab
Dankotchev <dankotchev.tech@outlook.com>
2023-03-08 10:41:09 -0300

    Sala de aula? 2023.03.08 - Usando tabelas

---
+++ FP2C5 - 00 Aulas Iniciais/03 Tables/src/java/br/edu/ifsp/pep/tables/model/Produto.java
@@ -0,0 +1,105 @@
+package br.edu.ifsp.pep.tables.model;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable{
+
+    private int id;
+    private String code;
+    private String name;
+    private String description;
+    private String image;
+    private double price;
+    private String category;
+    private int quantity;
+    private int rating;
+
+    //
+    public Produto() {
+    }
+
+    public Produto(int id, String code, String name, String description, String image, double price, String category, int quantity, int rating) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+        this.category = category;
+        this.quantity = quantity;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+}
