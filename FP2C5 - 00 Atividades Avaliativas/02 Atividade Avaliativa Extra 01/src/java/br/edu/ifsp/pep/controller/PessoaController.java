bfa1fc8bc4ce1a6016ef909d86a5d357a9e117c1
Dankotchev <dankotchev.tech@outlook.com>
2023-04-12 10:06:52 -0300

    Sala de Aula - 2304.12 -- Correção da Atividade Avaliativa Extra 01

--- FP2C5 - 00 Atividades Avaliativas/02 Atividade Avaliativa Extra 01/src/java/br/edu/ifsp/pep/controller/PessoaController.java
+++ FP2C5 - 00 Atividades Avaliativas/02 Atividade Avaliativa Extra 01/src/java/br/edu/ifsp/pep/controller/PessoaController.java
@@ -2,7 +2,7 @@ package br.edu.ifsp.pep.controller;
 
 import br.edu.ifsp.pep.model.Pessoa;
 import br.edu.ifsp.pep.services.PessoaService;
-import jakarta.annotation.PostConstruct;
+import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import java.io.Serializable;
@@ -12,21 +12,17 @@ import util.Mensagem;
 @RequestScoped
 public class PessoaController implements Serializable {
 
+    @EJB
+    private PessoaService pessoaService;
     private Pessoa usuario = new Pessoa();
-    private final PessoaService pessoaService = new PessoaService();
 
     public void login() {
         Pessoa login = pessoaService.login(usuario.getLogin(), usuario.getSenha());
-        if (login.getId() != Integer.MIN_VALUE)
+        if (login != null)
             Mensagem.info("Seja bem vindo " + login.getNome() + "!");
         else
             Mensagem.error("Login e/ou Senha inválidos");
         usuario = new Pessoa();
-    }
-
-    @PostConstruct
-    private void init() {
-        pessoaService.carregarPessoa();
     }
 
     //
@@ -37,5 +33,4 @@ public class PessoaController implements Serializable {
     public void setUsuario(Pessoa usuario) {
         this.usuario = usuario;
     }
-
 }
--- FP2C5 - 00 Atividades Avaliativas/02 Atividade Avaliativa Extra 01/src/java/br/edu/ifsp/pep/services/PessoaService.java
+++ FP2C5 - 00 Atividades Avaliativas/02 Atividade Avaliativa Extra 01/src/java/br/edu/ifsp/pep/services/PessoaService.java
@@ -1,34 +1,36 @@
 package br.edu.ifsp.pep.services;
 
 import br.edu.ifsp.pep.model.Pessoa;
+import jakarta.ejb.Stateless;
 import jakarta.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Named
+@Stateless
 public class PessoaService implements Serializable {
 
     private List<Pessoa> pessoas;
 
     public Pessoa login(String login, String senha) {
-        // Um objeto com menor valor de id possivel, para caso não encontre
-        //  ter a possibilidade de retornar um erro
-        Pessoa p = new Pessoa(Integer.MIN_VALUE);
         for (Pessoa pessoa : pessoas) {
             if (pessoa.getLogin().equals(login)
                     && pessoa.getSenha().equals(senha)) {
-                p = pessoa;
+                return pessoa;
             }
         }
-        return p;
+        return null;
     }
 
-    public void carregarPessoa() {
-        pessoas = new ArrayList<>();
+    private void carregarPessoa() {
+        this.pessoas = new ArrayList<>();
         for (int i = 1; i < 51; i++) {
             pessoas.add(new Pessoa(i, "Pessoa " + i, "login" + i, "senha" + i));
         }
     }
 
+    public PessoaService() {
+        this.carregarPessoa();
+    }
 }
