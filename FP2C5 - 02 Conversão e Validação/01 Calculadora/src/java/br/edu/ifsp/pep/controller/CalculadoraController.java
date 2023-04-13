26cdc290b0975ffbf33d8b59b352fc509127ff96
Dankotchev <dankotchev.tech@outlook.com>
2023-04-13 10:46:57 -0300

    Sala de aula - 2304.13 -- Iniciando validações

---
+++ FP2C5 - 02 Conversão e Validação/01 Calculadora/src/java/br/edu/ifsp/pep/controller/CalculadoraController.java
@@ -0,0 +1,42 @@
+package br.edu.ifsp.pep.controller;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class CalculadoraController {
+
+    private Double valorA;
+    private Double valorB;
+    private Double resultado;
+
+    public void somar() {
+        this.resultado = this.valorA + this.valorB;
+    }
+
+    //
+    public Double getValorA() {
+        return valorA;
+    }
+
+    public void setValorA(Double valorA) {
+        this.valorA = valorA;
+    }
+
+    public Double getValorB() {
+        return valorB;
+    }
+
+    public void setValorB(Double valorB) {
+        this.valorB = valorB;
+    }
+
+    public Double getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Double resultado) {
+        this.resultado = resultado;
+    }
+}
