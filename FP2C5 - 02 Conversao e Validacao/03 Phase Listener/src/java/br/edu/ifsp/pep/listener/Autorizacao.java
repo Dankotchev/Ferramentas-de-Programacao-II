6204843a8cbe5afa2be8e6b6b076ba963a7c257e
Dankotchev <dankotchev.tech@outlook.com>
2023-05-03 10:45:15 -0300

    Sala de Aula - 2305.03 -- Autenticação

---
+++ FP2C5 - 02 Conversao e Validacao/03 Phase Listener/src/java/br/edu/ifsp/pep/listener/Autorizacao.java
@@ -0,0 +1,53 @@
+package br.edu.ifsp.pep.listener;
+
+import br.edu.ifsp.pep.controller.PessoaController;
+import br.edu.ifsp.pep.model.Pessoa;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.PhaseEvent;
+import jakarta.faces.event.PhaseId;
+import jakarta.faces.event.PhaseListener;
+import jakarta.inject.Inject;
+import java.io.IOException;
+
+public class Autorizacao implements PhaseListener {
+
+    @Inject
+    private PessoaController pessoaController;
+
+    @Override
+    public void afterPhase(PhaseEvent event) {
+        System.out.println(" Após (after): " + event.getPhaseId());
+
+        FacesContext facesContext = event.getFacesContext();
+        String pagina = facesContext.getViewRoot().getViewId();
+        Pessoa pessoaAutenticada = pessoaController.getPessoaAutenticada();
+
+        System.out.println("Página atual: " + pagina);
+
+        if ((pessoaAutenticada == null
+                || !pessoaAutenticada.getTipo().equals("1"))
+                && pagina.equals("/financeiro/folhaPagamento.xhtml")) {
+            System.out.println("Redirecionando...");
+            try {
+                facesContext.getExternalContext()
+                        .redirect("/03%20Phase%20Listener/index.xhtml");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+//        HttpServletRequest request = (HttpServletRequest) facesContext
+//                .getExternalContext()
+//                .getRequest();
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent event) {
+        System.out.println(" Antes (before): " + event.getPhaseId());
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.ANY_PHASE;
+    }
+}
