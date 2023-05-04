1d12f166e6f89ecad3c65e29ea39215b7c1bf272
Dankotchev <dankotchev.tech@outlook.com>
2023-05-04 10:05:27 -0300

    Sala de Aula - 2305.04 -- Autenticação e redirecionamento usando NavigationHandler

--- FP2C5 - 02 Conversão e Validação/03PhaseListener/src/java/br/edu/ifsp/pep/listener/Autorizacao.java
+++ FP2C5 - 02 Conversão e Validação/03PhaseListener/src/java/br/edu/ifsp/pep/listener/Autorizacao.java
@@ -2,6 +2,7 @@ package br.edu.ifsp.pep.listener;
 
 import br.edu.ifsp.pep.controller.PessoaController;
 import br.edu.ifsp.pep.model.Pessoa;
+import jakarta.faces.application.NavigationHandler;
 import jakarta.faces.context.FacesContext;
 import jakarta.faces.event.PhaseEvent;
 import jakarta.faces.event.PhaseId;
@@ -18,22 +19,34 @@ public class Autorizacao implements PhaseListener {
     public void afterPhase(PhaseEvent event) {
         System.out.println(" Após (after): " + event.getPhaseId());
 
-        FacesContext facesContext = event.getFacesContext();
-        String pagina = facesContext.getViewRoot().getViewId();
+        if (event.getPhaseId() == PhaseId.APPLY_REQUEST_VALUES)
+            System.out.println("Após a fase de Apply Request Values");
+
+        FacesContext context = event.getFacesContext();
+        String pagina = context.getViewRoot().getViewId();
         Pessoa pessoaAutenticada = pessoaController.getPessoaAutenticada();
 
         System.out.println("Página atual: " + pagina);
 
+        // Bloqueando páginas da seção Financeira do projeto. Limitando apenas ao
+        //  tipo permitido
         if ((pessoaAutenticada == null
                 || !pessoaAutenticada.getTipo().equals("1"))
-                && pagina.equals("/financeiro/folhaPagamento.xhtml")) {
+                && pagina.startsWith("/financeiro/")) {
             System.out.println("Redirecionando...");
-            try {
-                facesContext.getExternalContext()
-                        .redirect("/03PhaseListener/index.xhtml");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+            // Redirecionando usando métodos do facesContexts
+//            try {
+//                context.getExternalContext()
+//                        .redirect("/03PhaseListener/index.xhtml");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            // Outra forma de redirecionar páginas, usando NavigationHandler
+            NavigationHandler navigationHandler = context.getApplication()
+                    .getNavigationHandler();
+
+            navigationHandler.handleNavigation(context, null, "erro");
         }
     }
 
