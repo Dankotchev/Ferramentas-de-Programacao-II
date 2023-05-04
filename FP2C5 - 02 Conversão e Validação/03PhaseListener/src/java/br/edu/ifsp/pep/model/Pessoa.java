411288106f46c2f2a8960667a3f7abe0ce00d91f
Dankotchev <dankotchev.tech@outlook.com>
2023-05-04 10:42:05 -0300

    Sala de Aula - 2305.04 -- BeanValidation

--- FP2C5 - 02 Conversão e Validação/03PhaseListener/src/java/br/edu/ifsp/pep/model/Pessoa.java
+++ FP2C5 - 02 Conversão e Validação/03PhaseListener/src/java/br/edu/ifsp/pep/model/Pessoa.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "pessoa")
@@ -13,12 +14,19 @@ public class Pessoa implements Serializable {
 
     @Id
     @Column(name = "login", length = 15, nullable = false)
+    @Size(min = 3, max = 15)
     private String login;
+
     @Column(name = "senha", length = 15, nullable = false)
+    @Size(min = 3, max = 15)
     private String senha;
+
     @Column(name = "nome", length = 40, nullable = false)
+    @Size(min = 3, max = 40)
     private String nome;
+
     @Column(name = "tipo", length = 3, nullable = false)
+    @Size(min = 1, max = 3)
     private String tipo;
 
     //
